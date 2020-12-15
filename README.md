0f502489bc9a92aa5c07c190fb086e8e9ba55746
atharva naik <atharvanaik2018@gmail.com>
2020-12-16 01:44:39 +0530

    small README fix

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # social_media
 A library for automating several social media actions, such as posting, login, notifications etc. for multiple  social media and scraping data from these social medias. API free (no official social media APIs used), front-end selinium based solutions only.
 ### Social media platforms supported
+<img style="display: inline;" src="https://github.com/atharva-naik/social_media/blob/main/images/facebook.png?raw=true" width="100">
 <img style="display: inline;" src="https://github.com/atharva-naik/social_media/blob/main/images/gmail.png?raw=true" width="100">
 <img style="display: inline;" src="https://github.com/atharva-naik/social_media/blob/main/images/hangouts.png?raw=true" width="100">
 <img style="display: inline;" src="https://github.com/atharva-naik/social_media/blob/main/images/instagram.jpg?raw=true" width="100">
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="social_media-Atharva1797", # Replace with your own username
-    version="0.0.1",
+    name="social_media", 
+    version="0.0.2",
     author="Atharva Naik",
     author_email="atharvanaik2018@gmail.com",
     description="Python package for scraping and automation of social media activity",
@@ -15,8 +15,9 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD 3 License",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
+    install_requires=['tqdm', 'nltk', 'numpy', 'spacy', 'pandas', 'sklearn', 'datetime', 'selenium', 'ansicolors', 'prettytable', 'webdriver_manager'],
     python_requires='>=3.6',
 )
